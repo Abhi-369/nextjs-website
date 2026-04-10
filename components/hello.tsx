c72699880daacbc133af4d75d7b5259886270081
abhi-087 <abhi.prajapati@nattyhatty.com>
2026-04-10 14:54:53 +0530

    first-commit

---
+++ components/hello.tsx
@@ -0,0 +1,12 @@
+'use client';
+
+const Hello = () => {
+    console.log('Hello')
+    return (
+        <div>
+            <h3 className={'text-red-500'}>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Rerum, vel.</h3>
+            Hello
+        </div>
+    )
+}
+export default Hello
